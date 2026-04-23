e21e7b17d39a664187b0fb820714499be06aa796
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-04-23 23:26:52 +0530

    fix: use google DNS for Atlas SRV resolution, fix OtP email config

--- server/server.js
+++ server/server.js
@@ -7,10 +7,14 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const dns = require('dns');
 const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config();
+
+// Use Google DNS to resolve MongoDB Atlas SRV records (fixes ISP DNS issues)
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 // Connect to MongoDB
 connectDB();
