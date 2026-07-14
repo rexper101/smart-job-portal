c278f5ca4ce09ff70aa6cb875f84f866f016f2b9
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-07-14 22:17:21 +0530

    improvement added

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is not defined in environment variables.');
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
